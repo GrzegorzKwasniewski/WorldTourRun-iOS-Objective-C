cd3a0b1f488bc4b90a64e0e66a13403adc893321
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-03-08 20:53:40 +0100

    Create Basic Implementation For Parsing JSON Data From Text File

--- WorldTourRun/CityTrophyController.m
+++ WorldTourRun/CityTrophyController.m
@@ -7,10 +7,57 @@
 //
 
 #import "CityTrophyController.h"
+#import "CityTrophy.h"
+
+@interface CityTrophyController()
+
+@property (strong, nonatomic) NSArray *trophies;
+
+@end
 
 @implementation CityTrophyController
 
-//+(CityAwardsController *)sharedInstance;
++(CityTrophyController *)sharedInstance {
+
+    static CityTrophyController *ctc;
+    if (ctc == nil) {
+        ctc = [[CityTrophyController alloc] init];
+        ctc.trophies = [self trophiesCollection];
+    }
+    
+    return ctc;
+    
+}
+
++(NSArray *)trophiesCollection {
+
+    NSString *fileLocation = [[NSBundle mainBundle] pathForResource:@"cities" ofType:@".txt"];
+    NSString *jsonData = [NSString stringWithContentsOfFile:fileLocation encoding:nil error:nil];
+    
+    NSData *data = [jsonData dataUsingEncoding:NSUTF8StringEncoding];
+    NSArray *trophiesFromJSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    
+    NSMutableArray *trophiesCollection = [NSMutableArray array];
+    
+    for (NSDictionary *trophy in trophiesFromJSON) {
+        
+        [trophiesCollection addObject:[self parsTrophy:trophy]];
+        
+    }
+    
+    return trophiesCollection;
+}
+
++(CityTrophy *)parsTrophy:(NSDictionary *)dictionary {
+    
+    CityTrophy *cityTrophy = [CityTrophy new];
+    cityTrophy.cityName = [dictionary objectForKey:@"name"];
+    cityTrophy.cityInformation = [dictionary objectForKey:@"information"];
+    cityTrophy.cityImageName = [dictionary objectForKey:@"imageName"];
+    cityTrophy.distanceToGetTrophy = [[dictionary objectForKey:@"distance"] floatValue];
+    return cityTrophy;
+    
+}
 
 @end
 
