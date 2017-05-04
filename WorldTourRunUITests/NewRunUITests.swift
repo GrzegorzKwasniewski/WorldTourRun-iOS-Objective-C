c000cedf1a46c3f1fb6d3c31d5c8f56a384686e8
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-05-04 14:17:42 +0200

    Test UI - Save New Run After Finish

--- WorldTourRunUITests/NewRunUITests.swift
+++ WorldTourRunUITests/NewRunUITests.swift
@@ -9,28 +9,47 @@
 import XCTest
 
 class NewRunUITests: XCTestCase {
+    
+    let app = XCUIApplication()
         
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+
         XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_SavingNewRunAfterFinish() {
+        
+        let newRunButton = app.buttons["New Run"]
+        newRunButton.tap()
+        
+        let startButton = app.buttons["Start"]
+        
+        XCTAssertEqual(startButton.isEnabled, true)
+        XCTAssertEqual(startButton.isSelected, false)
+
+        startButton.tap()
+        
+        let stopButton = app.buttons["Stop"]
+        
+        XCTAssertEqual(stopButton.isEnabled, true)
+        
+        stopButton.tap()
+        
+        XCTAssertEqual(stopButton.isSelected, false)
+        
+        let saveSheetButton = app.sheets.buttons["Save"]
+        saveSheetButton.tap()
+        
+        XCTAssertEqual(saveSheetButton.exists, false)
+        
     }
     
 }
