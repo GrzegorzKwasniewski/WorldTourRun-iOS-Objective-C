851424221d96430532e403907e065a5f1e78aec5
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-05-10 18:28:52 +0200

    Test UI - Add New Run Reminder

---
+++ WorldTourRunUITests/RunReminders.swift
@@ -0,0 +1,41 @@
+//
+//  RunReminders.swift
+//  WorldTourRun
+//
+//  Created by Grzegorz on 10/05/2017.
+//  Copyright © 2017 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import XCTest
+
+class RunReminders: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+
+        continueAfterFailure = false
+
+        XCUIApplication().launch()
+
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func test_AddNewRunReminder() {
+        
+        let app = XCUIApplication()
+        app.buttons["Run Reminders"].tap()
+        app.navigationBars["Run Reminders"].buttons["Add"].tap()
+        
+        let newRunEventAlert = app.alerts["New Run Event"]
+        newRunEventAlert.collectionViews.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .textField).element.typeText("run")
+        newRunEventAlert.buttons["Save"].tap()
+        app.tables.buttons["Reminde me"].tap()
+        app.alerts["Reminder for Your Run was added!"].buttons["OK"].tap()
+        
+        
+    }
+    
+}
