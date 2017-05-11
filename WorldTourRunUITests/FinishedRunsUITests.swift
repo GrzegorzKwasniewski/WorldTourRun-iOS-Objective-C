7c432098995d5b95a11fdd1cccb5fbd606b80426
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-05-11 20:50:52 +0200

    Test UI - Delete Finished Run From Database

---
+++ WorldTourRunUITests/FinishedRunsUITests.swift
@@ -0,0 +1,38 @@
+//
+//  FinishedRunsUITests.swift
+//  WorldTourRun
+//
+//  Created by Grzegorz on 11/05/2017.
+//  Copyright © 2017 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import XCTest
+
+class FinishedRunsUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+
+        continueAfterFailure = false
+
+        XCUIApplication().launch()
+
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+        let app = XCUIApplication()
+        app.buttons["Finished Runs"].tap()
+        
+        let cell = app.tables.cells.element(boundBy: 0)
+        
+        if cell.exists {
+            cell.swipeLeft()
+            cell.children(matching: .button).matching(identifier: "Delete").element(boundBy: 0).tap()
+        }
+    }
+}
